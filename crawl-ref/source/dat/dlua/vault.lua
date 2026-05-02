Mu-L/crawl

commit c79536f93dbb9f9136d944b518109c9e25660133

Another quick and brief vault review (+ a minor Zig change)
Worthy of note:

 * pf_dread_powers_of_hell has constantly gotten confused reports of it
   deploying reapers and balrugs not wielding their iconic weapons, as
   both of them haven't kept the wands of flame they were supposed to
   be given by the vault for multiple years. Rather than revert it to
   the state prior to a1918b6 and end up using crimson imps in Depths,
   I am opting to remove the monsters from the vault entirely, which is
   a mild shame but fairly less confusing.

 * A few Gulch monsters have been added to the usual hyper-randomized
   Sprint trio (arenasprint, zigsprint, and pitsprint) as are closest to
   contemporary design principles.

 * I've made another new Arena map (the testing one, not the Oka one)
   with 2-tile-wide corridors to test performances beyond open spaces
   or 1-tile-wide corridors.

 * hellmonk_guarded_unrand_damnation no longer uses hellions in Elf,
   partially because I've got mixed feelings about directly deploying
   demons in the branch with such diminished demonology themes, and
   partially so one of the best ranged weapon unrands can actually be
   guarded by one of the most dangerous ranged monster in the game.

 * Ziggurats past the first one cleared redistribute the weight of potions
   of experience over to potions of mutation, as the way potions of
   experience are currently implemented makes them give very little
   besides the experience level by the time one enters a zig, and
   rerolling mutations still holds some interest for megazig divers.

crawl-ref/source/dat/des/branches/zot.des
@@ -3840,7 +3840,7 @@ SHUFFLE: ])deOG, OGUCD~, GM
 : tile("b = dngn_crystal_" .. wallc)
 : tile("v = dngn_metal_wall_" .. wallc)
 : tile("c = dngn_stone_wall_" .. wallc)
-: ftile(".`123409deO%CDTG~+ = floor_rough_" .. floorc)
+: ftile(".`^123409deO%CDTG~+ = floor_rough_" .. floorc)
 : vault_granite_statue_setup(_G, "G", "scintillating statue")
 : vault_metal_statue_setup(_G, "M", "enigmatic dynamo")
 MAP

commit 02af3d50c8ca72feb466625fcf3f7fdd5e477b9d

Improve vault itemspec error handling, fix found errors (DracoOmega)
Crawl's item specification and definition error handling has had
extremely significant errors in it since 0.16. The way it parses items in
a list will silently pass by incorrect entries as long as there is another
valid item further in that list (since the error message it would report
gets replaced by the next work not having any errors). While failing
silently and gracefully allows vaults with some working definitions to
still place the working part of an item list, it's very noticeably against
the policy of Crawl not loading from other significant vault errors, and
it is exceedingly bad policy to completely hide failing item entries from
anybody working on Crawl for relatively indefinite periods of development.

This commit, with some assistance from DracoOmega, makes Crawl print
errors instead of loading whenever it actually parses that an incorrect
item has been passed to vault creation in a vault. I have also thus
gone through and fixed every incorrect item specification Crawl reports:
for the most part, this only fixes minor errors, but most notably it
reintroduces small chances of getting multiple ally scrolls in each Trove
post-rework and also catches the ziggurat potion of mutation syntax error
introduced in c79536f.

(Our limited means of error handling means that, unfortunately, we can't
have Crawl cleanly present every vault's syntax errors at once. It also
means we can't really try to, say, have Crawl report these issues to users
and then load in other valid item specifications or other vaults entirely
without stopping play dead. Further adjustments here may be beneficial,
but for now, this falling into the same means of handling as other .des
syntax errors is fairly more consistent and helps catch more actual
errors.)

crawl-ref/source/dat/des/altar/overflow.des
@@ -1040,7 +1040,7 @@ ENDMAP
 NAME:   nicolae_elyvilon_little_fluffy_clouds
 TAGS:   transparent decor
 WEIGHT: 7
-KITEM:  _ = none w:100 / orb / orb ego:light w:1
+KITEM:  _ = nothing w:100 / orb / orb ego:light w:1
 MARKER: _ = lua:fog_machine{cloud_type = "white fluffiness", pow_min = 10, \
              pow_max = 15, delay_min = 20, delay_max = 30, size = 1, \
              walk_dist = 3, spread_rate = 0}

crawl-ref/source/dat/des/branches/abyss.des
@@ -851,7 +851,7 @@ MONS:    rakshasa ; whip good_item | trident good_item | long sword good_item . 
                     ring mail good_item | kite shield good_item . \
                     mundane javelin | boomerang | throwing net
 MONS:    guardian serpent / great orb of eyes
-ITEM:    d = potion of curing pre_id / potion of moonshine pre_id
+ITEM:    potion of curing pre_id / potion of moonshine pre_id
 SUBST:   x = ccccvbb
 MAP
      .x.x

crawl-ref/source/dat/des/branches/lair.des
@@ -4116,8 +4116,8 @@ KMONS:   8 = raven
 KMONS:   R = spectral anaconda
 # A necromancy-themed loot item behind the obsidian statue.
 ITEM:    rapier ego:pain w:5 / rapier ego:draining w:5 / \
-         quickblade ego:pain w:5 / quickblade ego:draining w:5 / \
-         demon blade ego:draining / skull of zonguldruk w:1 / \
+         quick blade ego:pain w:5 / quick blade ego:draining w:5 / \
+         demon blade ego:draining / skull of zonguldrok w:1 / \
          great sword ego:draining / triple sword ego:draining / \
          demon whip ego:pain w:5 / demon whip ego:draining w:5 / \
          eveningstar ego:draining / sceptre of torment w:1 / \

crawl-ref/source/dat/des/branches/snake.des
@@ -2603,7 +2603,7 @@ KMONS:  12 = guardian serpent / naga w:20 / salamander / anaconda w:20 / \
 KMONS:  3 = naga mage / naga ritualist / naga sharpshooter / shock serpent / \
             salamander mystic / nothing w:40
 KMONS:  4 = nagaraja / salamander tyrant
-KITEM:  2 = %:15 / * / |:5 / nothing w:5
+KITEM:  2 = % w:15 / * / | w:5 / nothing w:5
 NSUBST: 4 = 2=4 / 34
 SUBST:  D = 13
 MAP

crawl-ref/source/dat/des/branches/vaults_rooms_hard.des
@@ -38,8 +38,8 @@ function init_protected_by_tso(e)
   e.tags('vaults_hard')
   e.kmons('A = angel')
   e.kmons('1 = generate_awake daeva')
-  e.kmons('2 = vault warden ; great sword ' .. h .. ' | battleaxe .. ' .. h ..
-                             'glaive ' .. h .. ' | great mace ' .. h .. ' . ' ..
+  e.kmons('2 = vault warden ; great sword ' .. h .. ' | battleaxe ' .. h ..
+                             ' | glaive ' .. h .. ' | great mace ' .. h ..
                              ' . chain mail | plate armour')
   e.kmons('3 = generate_awake vault guard / \
                generate_awake ironbound mechanist w:2')
@@ -2758,7 +2758,7 @@ NAME:    nicolae_vaults_lava_storage_facility
 KMONS:   1 = yaktaur captain ; arbalest ego:flaming pre_id w:15 | \
              hand cannon ego:flaming pre_id w:3
 : local f = "great sword ego:flaming pre_id | long sword ego:flaming pre_id |"..
-: " battle axe ego:flaming pre_id | war axe ego:flaming pre_id | great mace " ..
+: " battleaxe ego:flaming pre_id | war axe ego:flaming pre_id | great mace " ..
 : "ego:flaming pre_id | dire flail ego:flaming pre_id | bardiche ego:flaming "..
 : "pre_id | glaive ego:flaming pre_id . ring mail ego:fire_resistance pre_id|"..
 : "chain mail ego:fire_resistance pre_id | " ..

crawl-ref/source/dat/des/branches/zot.des
@@ -3900,7 +3900,7 @@ KMONS:  O = diamond obelisk
 KMONS:  S = lightning spire
 ITEM:   storm talisman, randbook disc:air spells:chain_lightning
 : local e = "ego:electrocution good_item pre_id no_pickup"
-: item("quickblade " .. e .. " w:5 / double sword " .. e .. " w:5 / " ..
+: item("quick blade " .. e .. " w:5 / double sword " .. e .. " w:5 / " ..
 :      "executioner's axe " .. e .. " w:5 / eveningstar " .. e .. " w:5 / " ..
 :      "bardiche " .. e .. " w:5 / lajatang " .. e .. " w:5 / " ..
 :      "rapier " .. e .. " / scimitar " .. e .. " / dire flail " .. " / " ..

crawl-ref/source/dat/des/portals/gauntlet.des
@@ -1108,7 +1108,7 @@ kitem("P = felid skeleton, protean talisman randart / eel talisman / " ..
           "any scroll / any potion / nothing w:60")
 
 -- Barachi warper.
-kitem("Q = barachi skeleton, book of spatial translocation / " ..
+kitem("Q = barachi skeleton, book of spatial translocations / " ..
           "book of displacement / there-and-back book / book of the warp / " ..
           "randbook disc:translocation, " ..
           "scroll of blinking / scroll of teleportation q:2 / " ..

crawl-ref/source/dat/des/portals/gulch.des
@@ -133,7 +133,7 @@ function gulch_loot(e, item_nsubst, potion_count)
   local schools1 = util.random_from({"alchemy", "hexes", "forgecraft"})
   local schools2 = util.random_from({"fire", "ice", "air", "earth"})
   local orbprop = util.random_from({"Alch", "Forge", "rPois", "rCorr"})
-  local weapon_string = "d = "
+  local weapon_string = ""
   for _, wt in ipairs(weapon) do
     weapon_string = weapon_string .. " / " .. wt .. " ego:venom pre_id good_item"
   end
@@ -157,7 +157,7 @@ function gulch_loot(e, item_nsubst, potion_count)
                "hand cannon good_item w:20")
   e.kitem("e = staff of alchemy randart artprops:Hexes w:5 / " ..
           "staff of alchemy randart artprops:Forge w:5 / " ..
-          "randbook disc:" .. schools1 .. " disc2:" .. schools2 ..
+          "randbook disc:" .. schools1 .. " disc2:" .. schools2 .. " " ..
           "splevels:33 numspells:5 / " ..
           "orb ego:attunement randart artprops:" .. orbprop .. " w:3 / " ..
           "orb ego:stardust randart artprops:" .. orbprop .. " w:3 / " ..
@@ -242,8 +242,8 @@ function gulch_crossways_sections(e)
   e.kmons("5 = fungal shambler / formless jellyfish w:6 / " ..
               "telencephalon w:4 / torpor snail w:1")
   e.subst("~ = ..Wwx")
-  gulch_generic_subvaults(e, {"y", "z"}, "x.", true)
   gulch_boss_theme_sets(e, false)
+  gulch_generic_subvaults(e, {"y", "z"}, "x.", true)
   gulch_loot(e, "|", 4)
 end
 
@@ -256,8 +256,8 @@ function gulch_network(e)
   e.kmons("4 = great orb of eyes / crawling flesh cage / telencephalon w:2")
   e.kmons("5 = fungal shambler / formless jellyfish w:6 / " ..
               "telencephalon w:4 / torpor snail w:1")
-  gulch_generic_subvaults(e, {"y", "z"}, "xxW..", true)
   gulch_boss_theme_sets(e, false)
+  gulch_generic_subvaults(e, {"y", "z"}, "xxW..", true)
   gulch_loot(e, "|", 4)
 end
 
@@ -270,7 +270,6 @@ function gulch_chamber_river(e, is_subvault)
   e.kmons("4 = great orb of eyes / crawling flesh cage / telencephalon w:2")
   e.kmons("5 = fungal shambler / formless jellyfish w:6 / " ..
               "telencephalon w:4 / torpor snail w:1")
-  gulch_generic_subvaults(e, {"y", "z", "Y", "Z"}, "xxW..", true)
   if crawl.one_chance_in(3) then
     e.kmons("? = fungus")
     e.subst("? : ;WW?")
@@ -281,6 +280,7 @@ function gulch_chamber_river(e, is_subvault)
   if is_subvault then
     gulch_loot(e, "|", 3)
   end
+  gulch_generic_subvaults(e, {"y", "z", "Y", "Z"}, "xxW..", true)
 end
 
 function gulch_winding_coils(e)

crawl-ref/source/dat/des/portals/necropolis.des
@@ -4507,7 +4507,7 @@ ITEM:    any magical staff / quarterstaff randart ego:spectral w:5 pre_id / \
          dagger randart plus:9 pre_id w:5 / rapier good_item / \
          randbook disc:hexes disc2:summoning spells:summon_forest numspells:3 / \
          swamp dragon scales no_pickup / any ally scroll q:2 pre_id
-ITEM:    any staff randart artprops:Hexes / \
+ITEM:    any magical staff randart artprops:Hexes / \
          lajatang randart ego:spectral w:5 pre_id / \
          athame randart w:2 / quick blade good_item / \
          randbook disc:hexes disc2:summoning spells:summon_forest numspells:6 / \
@@ -4695,7 +4695,7 @@ NSUBST:  ' = 1:1 / 1:2 / *:', " = 2:1 / *:"
 NSUBST:  $ = 2:d / *:%, * = 1:e / 1:f, % = 2:* / *:%
 : end
 KMONS:   O = player ghost
-ITEM:    dagger ego:vamprism pre_id / falchion ego:vampirism pre_id / \
+ITEM:    dagger ego:vampirism pre_id / falchion ego:vampirism pre_id / \
          potion of curing q:1 pre_id w:20 / scroll of fog q:1 pre_id w:5 / \
          amulet of regeneration pre_id w:5
 ITEM:    rapier ego:vampirism pre_id / scimitar ego:vampirism pre_id / \
@@ -4754,7 +4754,7 @@ SUBST:   ?! = %
 KMONS:   O = player ghost
 ITEM:    staff of fire / staff of necromancy / quarterstaff ego:spectral / \
          quarterstaff ego:pain / any ring randart artprops:rN pre_id w:15
-ITEM:    staff of fire randart / staff of pain randart / \
+ITEM:    staff of fire randart / staff of necromancy randart / \
          lajatang ego:spectral / lajatang ego:pain / \
          any amulet randart artprops:Regen&&rN pre_id w:15
 ITEM:    scarab talisman, scarab talisman randart

crawl-ref/source/dat/des/portals/trove.des
@@ -312,7 +312,7 @@ function trove_standard_magic(e, school, use_staff)
     ["conjuration"] = {"conjuration", "Conj", "conjurations"},
     ["summoning"] = {"ERROR: set use_staff to false", "Summ", "summonings"},
     ["necromancy"] = {"necromancy", "Necro", "necromancy"},
-    ["translocations"] = {"ERROR: set use_staff to false", "Tloc", "translocations"},
+    ["translocation"] = {"ERROR: set use_staff to false", "Tloc", "translocations"},
     ["hexes"] = {"ERROR: set use_staff to false", "Hexes", "hexes"},
     ["fire"] = {"fire", "Fire", "fire magic"},
     ["ice"] = {"cold", "Ice", "ice magic"},
@@ -1092,7 +1092,8 @@ local box = nil
 local talis = nil
 local magic = trove_standard_magic(_G, "alchemy", true)
 if no_friendlies(_G) == false then
-  table.insert(scroll, "any ally scroll q:2")
+  table.insert(scroll, "butterflies w:5 q:2")
+  table.insert(scroll, "summoning w:5 q:2")
   box = "box of beasts w:5"
   talis = "hive talisman"
   if you.mutation("no summoning magic") ~= 1 then
@@ -1217,7 +1218,7 @@ elseif you.mutation("hooves") ~= 3 and you.mutation("talons") ~= 3 then
             "pair of boots ego:rampaging good_item w:2"
 end
 if not no_evil(e) then
-  talis = " w:30 / sanguine talisman"
+  talis = "sanguine talisman"
 end
 
 trove_offense(_G, trove_weap_brand(_G, "venom", false) .. " / quick blade randart / " ..
@@ -1298,7 +1299,7 @@ if you.species() ~= "Demigod" then
 end
 kitem('d = ' .. weap)
 kitem('e = ' .. manual)
-kitem('f = any ally scroll q:2')
+kitem('f = scroll of summoning q:2')
 trove_defense(_G, "pearl dragon scales good_item / \
                    pearl dragon scales randart artprops:*Silence&Will w:5",
                    trove_ego_gen_armour(_G, "willpower", "Will"), jewelery,
@@ -1376,7 +1377,7 @@ if you.mutation("no potions") ~= 1 then
                      potion of heal wounds pre_id q:2"
 end
 if no_friendlies(_G) == false then
-  cons = cons .. " / any ally scroll q:4 w:2"
+  cons = cons .. " / scroll of summoning q:3 w:2"
 end
 kitem("h = " .. cons)
 kitem("i = scroll of brand weapon q:2 pre_id")
@@ -1530,10 +1531,10 @@ MONS:    spectral golden dragon, spectral quicksilver dragon
 -- All dragon scales, some elemental books, some elemental / slay jewellery,
 -- hats and a manual of invocations to match the Mask, some consumables,
 -- and some decoration.
-local dg = "dragon_scales good_item"
+local dg = "dragon scales good_item"
 local dr = "dragon scales randart"
 kitem("d = steam " .. dr .. " / acid " .. dr .. " / swamp " .. dg .. " w:7 / \
-           swamp" .. dr .. " / quicksilver dragon scales w:2 / \
+           swamp " .. dr .. " / quicksilver dragon scales w:2 / \
            quicksilver " .. dr .. " w:1")
 kitem("e = fire " .. dg .. " w:6 / fire " .. dr .. " w:2 / \
            ice " .. dg .. " w:6 / ice " .. dr .. " w:2 / \
@@ -1541,7 +1542,7 @@ kitem("e = fire " .. dg .. " w:6 / fire " .. dr .. " w:2 / \
 kitem("f = storm " .. dg .. " w:6 / storm " .. dr .. " w:4 / \
            shadow " .. dg .. " w:6 / shadow " .. dr .. " w:4 / \
            golden " .. dg .. " w:4 / golden " .. dr .. " w:2")
-kitem("g = serpent tailsman randart w:2 / dragon-coil talisman w:6 / \
+kitem("g = serpent talisman randart w:2 / dragon-coil talisman w:6 / \
            dragon-coil talisman randart w:12")
 local book = trove_book(_G, "fire", "ice") .. " / " ..
              trove_book(_G, "air", "earth")
@@ -1552,7 +1553,7 @@ kitem("h = " .. book)
 kitem("i = ring of slaying randart w:15 / any amulet randart artprops:rF / \
            any amulet randart artprops:rC / any amulet randart artprops:rPois")
 kitem("j = useful hat ego:see_invisible good_item w:4 / \
-           useful hat ego_willpower good_item w:4 / \
+           useful hat ego:willpower good_item w:4 / \
            hat randart artprops:Will / hat randart artprops:Slay")
 kitem("k = " .. trove_cons(_G, {"fear q:2", "enchant armour w:15 q:2"},
                           {"berserk rage q:2", "enlightenment q:2"},
@@ -1638,7 +1639,7 @@ NSUBST: ? = 6:d / 6:g / *:'
 {{
 local weap = ""
 local aux1 = ""
-local magic = trove_standard_magic(_G, "translocations", false)
+local magic = trove_standard_magic(_G, "translocation", false)
 local extra = "any manual w:1"
 if no_evil(_G) then
   weap = "acquire weapon / any useful weapon good_item"
@@ -1751,7 +1752,7 @@ local magic = trove_standard_magic(_G, "earth", true)
 local aux = ""
 local extra = ""
 if no_friendlies(_G) == false then
-  extra = extra .. " / any ally scroll q:2"
+  extra = extra .. " / scroll of butterflies q:2"
   if you.mutation("no summoning magic") ~= 1 then
     magic = magic .. " / " .. trove_standard_magic(_G, "summoning", false) ..
             " / any magical staff randart artprops:Summ&Will w:5"

crawl-ref/source/dat/des/portals/volcano.des
@@ -1257,7 +1257,7 @@ WEIGHT: 20
 MONS:   steam dragon / steam dragon / smoke demon / hell hound
 MONS:   sun demon, lindwurm
 KMONS:  4 = fire crab / fire dragon w:5
-KITEM:  4 = scroll of fog q:2 pre_id w:20 / condensation vane / \
+KITEM:  4 = scroll of fog q:2 pre_id w:20 / condenser vane / \
             cloak of the thief w:1
 SUBST:  a = ..xxl, p = ..'xl, q = .x, ' = '.
 NSUBST: 1 = 1:1 / 2:. / * = 1.., D2 = 2:., $ = 1:4 / *:$

crawl-ref/source/dat/des/portals/wizlab.des
@@ -785,7 +785,7 @@ MARKER:     ! = lua:fog_machine { \
 # player complete the rest of the map.
 ITEM:       potion of haste / potion of resistance w:5 / any potion w:20 \
             / scroll of silence w:5 / scroll of fog w:5 / any scroll w:20
-ITEM:       scarf ego:repulsion / book of vapours / condenser vane \
+ITEM:       scarf ego:repulsion / trismegistus codex / condenser vane \
             / staff of air
 # Main loot, including some thematic items that are good even if you don't care
 # about flight.
@@ -1290,7 +1290,7 @@ KMONS:      & = col:mutagenic primal ox-ox-weird mutant beast perm_ench:regen \
 ITEM:       potion of mutation q:1 pre_id
 ITEM:       randbook disc:alchemy disc2:necromancy owner:Cigotuvi
 ITEM:       protean talisman randart / maw talisman randart / \
-            serpent talisman randart / statue talisman w:8 / \
+            serpent talisman randart / granite talisman w:8 / \
             dragon-coil talisman / storm talisman w:8 / \
             talisman of death w:2
 ITEM:       staff of necromancy pre_id, scroll of poison pre_id
@@ -2164,9 +2164,9 @@ MONS:       patrolling electric golem
 KMONS:      P = lightning spire
 KMONS:      M = ironbound mechanist
 KMONS:      S = stack of scrap
-ITEM:       scarf ego:harm randart w:25 / boots ego:rampaging w:25 /\
-            boots ego:rampaging randart /\
-            any armour good_item ego:ponderous randart /\
+ITEM:       scarf ego:harm randart w:25 / pair of boots ego:rampaging w:25 /\
+            pair of boots ego:rampaging randart /\
+            any body armour good_item ego:ponderousness randart /\
             plate armour good_item ego:ponderousness / any weapon ego:chaos /\
             maxwell's patent armour w:2 / maxwell's thermic engine w:2
 ITEM:       any armour good_item w:20 / any weapon good_item w:20 /\

crawl-ref/source/dat/des/sprint/linesprint.des
@@ -219,7 +219,7 @@ KITEM:    a = potion of curing q:4 pre_id, \
 KITEM:    b = scroll of blinking q:1 pre_id, \
               scroll of amnesia q:2 pre_id
 KITEM:    d = any blast wand charges:10 pre_id /\
-              any beam wand charges:10 pre_id, \
+              any beam wand charges:10 pre_id
 KITEM:    e = rapier plus:2 ego:electrocution pre_id /\
            rapier plus:2 ego:draining pre_id /\
            rapier plus:2 ego:venom pre_id /\

crawl-ref/source/dat/des/sprint/pitsprint.des
@@ -707,7 +707,7 @@ KITEM: k = protean talisman / serpent talisman /\
            scale mail plus:4 pre_id /\
            plate armour plus:3 pre_id
 KITEM: l = any randbook slevels:14, /\
-       ring of protection plus:3 pre_id \
+       ring of protection plus:3 pre_id /\
        ring of evasion plus:3 pre_id /\
        ring of strength plus:3 pre_id /\
        ring of intelligence plus:3 pre_id /\

crawl-ref/source/dat/des/variable/float.des
@@ -4603,9 +4603,9 @@ NAME:   minmay_shoctagon
 DEPTH:  D:12-, Elf, Depths
 ORIENT: float
 ITEM:   book of air / book of lightning / book of storms / \
-        manual of air magic / staff of air pre_id / lightning rod w:20 \
+        manual of air magic / staff of air pre_id / lightning rod w:20 / \
         storm dragon scales w:5 / storm talisman w:5
-KITEM:  2 = arc blade / quick blade ego:electrocution randart pre_id \
+KITEM:  2 = arc blade / quick blade ego:electrocution randart pre_id / \
         glaive of the guard / \
         demon trident ego:electrocution randart pre_id / storm bow
 : if you.in_branch("D") then
@@ -7490,7 +7490,7 @@ MONS:    rakshasa, vampire knight, shadow dragon
 SUBST:   ' = 12, " = 56
 : end
 KITEM:   & = shadow dragon scales randart / manual of stealth / \
-             boots ego:stealth good_item pre_id / \
+             pair of boots ego:stealth good_item pre_id / \
              cloak ego:stealth good_item pre_id / \
              scroll of fog q:3 pre_id
 KFEAT:   B = cache_of_baked_goods

crawl-ref/source/dat/des/variable/grated_community.des
@@ -230,21 +230,21 @@ NSUBST: P = 1:P / *:G
 KMONS:  P = withered plant
 :   kitem("% = $, any wand / w:15 nothing, " .. s)
 :   kitem("* = scroll of torment pre_id q:" .. q .. " / nothing, " .. s)
-:   kitem("| = potion of magic pre_id q: " .. q .. " / nothing, " .. p)
+:   kitem("| = potion of magic pre_id q:" .. q .. " / nothing, " .. p)
 KFEAT:  C = altar_kikubaaqudgha / altar_sif_muna w:4
 SUBST:  & = ddddd., G = GGGGGY
 : elseif occupant == 1 then
 KMONS:  P = plant
 :   kitem("% = $, " .. p .. ", " .. p .. ", " ..p)
 :   kitem("% = scroll of vulnerability / nothing, " .. s)
-:   kitem("* = potion of magic pre_id q: " .. q .. " / nothing, " .. p)
+:   kitem("* = potion of magic pre_id q:" .. q .. " / nothing, " .. p)
 KFEAT:  C = altar_sif_muna / altar_ashenzari w:5
 SUBST:  1 = 2, & = eeeee., G = GTU
 : else
 KITEM:  % = $, any beam wand / any blast wand, \
             potion of brilliance pre_id / nothing
 :   kitem("* = scroll of immolation pre_id / nothing, " .. s)
-:   kitem("| = scroll of immolation pre_id q: " .. q .. " / nothing, " .. s)
+:   kitem("| = scroll of immolation pre_id q:" .. q .. " / nothing, " .. s)
 KFEAT:  C = altar_vehumet / altar_makhleb w:2
 KFEAT:  P = cache_of_fruit
 SUBST:  1 = 3, & = fffff.

crawl-ref/source/dat/des/variable/large_themed.des
@@ -2822,7 +2822,7 @@ TILE:    v = dngn_metal_wall_red
 FTILE:   ."~01234567Rs>V%$d*|-+abcdefABCDEFTUVn = floor_zot_diamonds
 : end
 KITEM:   & = demon whip / demon blade / demon trident / \
-             plate armor ego:fire_resistance pre_id / \
+             plate armour ego:fire_resistance pre_id / \
              plate armour ego:command pre_id w:5 / \
              fire dragon scales good_item / fire dragon scales randart / \
              sanguine talisman w:2 / dragon-coil talisman w:3
@@ -3514,8 +3514,9 @@ ITEM:    any scroll, parchment
 # Books with notable spells at level 6 or higher, across each school.
 ITEM:    book of spontaneous combustion / book of ice / book of spectacle / \
          book of power / book of chaos / book of the warp / book of death / \
-         book of siegecraft / necronomicon / grand grimoire / book of annihilations
-ITEM:    robe randart / orb ego:guile randart / orb ego:channeling randart / \
+         compendium of siegecraft / necronomicon / grand grimoire / \
+         book of annihilations
+ITEM:    robe randart / orb ego:guile randart / orb ego:energy randart / \
          orb ego:stardust randart / orb ego:attunement randart / \
          any magical staff randart w:15 / \
          ring of wizardry randart / manual of spellcasting w:5

crawl-ref/source/dat/des/variable/mini_monsters.des
@@ -487,7 +487,7 @@ NSUBST:  10 = 2:3 / 1:2, % = 1:| / 1:* / 1:e / * = *%%
 SUBST:   W = ., X = x
 : end
 ITEM:    throwing net q:1 no_pickup / rapier ego:venom no_pickup w:12 / \
-         scimitar ego:venom no_pickup w:8 / \ any hex wand charges:8 no_pickup / \
+         scimitar ego:venom no_pickup w:8 / any hex wand charges:8 no_pickup / \
          potion of ambrosia q:1 w:3 pre_id / potion of curing w:3 pre_id / \
          fen folio w:3 / my sojourn through swampland w:3 / \
          dart ego:curare no_pickup w:3 / scarab talisman
@@ -528,7 +528,7 @@ DEPTH:   D:3-7, Depths
 : if you.in_branch("D") then
 MONS:    bombardier beetle
 MONS:    orc wizard w:20 / orc wizard band w:15 / orc band / orc w:5
-KITEM:   any weapon ego:flaming w:18 / parchment disc:fire w:18 / \
+ITEM:    any weapon ego:flaming w:18 / parchment disc:fire w:18 / \
          wand of flame w:18 / scarab talisman w:18 / \
          ring of protection from fire w:18 / manual of fire magic w:15 / \
          fire dragon scales w:15 / pair of gloves ego:fire w:5 / \

crawl-ref/source/dat/dlua/vault.lua
@@ -245,15 +245,15 @@ function vault_species_skeletons(e, category)
   local s3 = {"barachi", "demigod", "dwarf", "demonspawn", "felid", "oni"}
   local output = "human skeleton"
   if category == "early" or category == "dungeon" or category == "all" then
-    output = output .. " / " .. table.concat(s1, " skeleton / ")
+    output = output .. " / " .. table.concat(s1, " skeleton / ") .. " skeleton"
   end
   if category == "late" or category == "dungeon" or category == "all" then
-    output = output .. " / " .. table.concat(s2, " skeleton / ")
+    output = output .. " / " .. table.concat(s2, " skeleton / ") .. " skeleton"
   end
   if category == "all" then
-    output = output .. " / " .. table.concat(s3, " skeleton / ")
+    output = output .. " / " .. table.concat(s3, " skeleton / ") .. " skeleton"
   end
-  return output  .. " skeleton"
+  return output
 end
 
 -- Three sets of reusable vault feature redefines scattered across the game,

crawl-ref/source/dat/dlua/ziggurat.lua
@@ -662,7 +662,7 @@ local function ziggurat_create_loot_at(c)
   local xpw = math.max(10, 190 - you.zigs_completed() * 30)
   local mtw = math.min(470, 290 + you.zigs_completed() * 30)
   local super_loot = dgn.item_spec("| no_pickup w:7000 /" ..
-                    "potion of experience no_pickup q:1 w: " .. xpw .. " / " ..
+                    "potion of experience no_pickup q:1 w:" .. xpw .. " / " ..
                     "potion of mutation no_pickup w:" .. mtw .. " / " ..
                     "potion of cancellation q:5 no_pickup / " ..
                     "potion of heal wounds q:5 no_pickup / " ..

crawl-ref/source/mapdef.cc
@@ -5890,8 +5890,8 @@ item_list::item_spec_slot item_list::parse_item_spec(string spec, bool ignore_ex
         item_spec parsed_spec;
         if (!parse_single_spec(parsed_spec, specifier))
         {
-            dprf(DIAG_DNGN, "Failed to parse: %s", specifier.c_str());
-            continue;
+            error = make_stringf("Error parsing '%s':\n%s", spec.c_str(), error.c_str());
+            break;
         }
         if (ignore_excluded
             || parsed_spec.props.exists(NO_EXCLUDE_KEY)